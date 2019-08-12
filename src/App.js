6aa3660b204d8d3095d0a944e4d1844912c68a97
mariusene98 <mariusene98@gmail.com>
2019-08-12 13:38:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Film(props) {
+  
+  return (
+
+    <div>
+      <img src={logo} className="App-logo" alt="logo" />
+    film: {props.film}
+    </div>
+    
+    )
+    
+
+}
+ function Nav(props) {
+   
+   return (
+     <div>
+       numele utilizatorului: {props.username}
+
+     </div>
+   )
+ }
+
+
 function App() {
+  var username = "marius";
+  var film = "X-MAN"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Film film={film}/>
+      <Nav username={username}/>  
     </div>
   );
 }
